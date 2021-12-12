b603831609b5037ab70e910bbd95e4cb379c1202
GURRA Ina <ina.gurra@akka.eu>
2021-12-13 00:53:17 +0100

    test

--- src/test/java/com/project/gutendex/GutendexControllerTest.java
+++ src/test/java/com/project/gutendex/GutendexControllerTest.java
@@ -1,32 +1,45 @@
 package com.project.gutendex;
 
+import com.project.gutendex.controller.GutendexController;
+import com.project.gutendex.entity.Book;
+import com.project.gutendex.entity.ResultBooks;
+import com.project.gutendex.service.GutendexService;
 import org.aspectj.lang.annotation.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
 
+import java.util.List;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
-public class GutendexControllerTest extends GutendexApplicationTests{
+@WebMvcTest(GutendexController.class)
+public class GutendexControllerTest {
 
     @Autowired
-    private WebApplicationContext webApplicationContext;
-
     private MockMvc mockMvc;
 
-    @Before("")
-    public void setup() {
-        mockMvc = MockMvcBuilders.webAppContextSetup(webApplicationContext).build();
+    @MockBean
+    private GutendexService service;
+
+    @Test
+    public void testRetrievingBooks() throws Exception {
+        ResultBooks resultBooks= new ResultBooks();
+        when(service.orderBooks()).thenReturn(resultBooks);
+
+        resultBooks.setResults(List.of(new Book("1","Pride and Prejudice",32),new Book("2","White nights",50)));
+
+        this.mockMvc.perform(get("/downloadCSV")).andDo(print()).andExpect(status().isOk())
+                .andExpect(content().contentType("text/csv"))
+                .andExpect(content().string(containsString("Pride and Prejudice")));
     }
-
-   /* @Test
-    public void testEmployee() throws Exception {
-        mockMvc.perform(get("/downloadCSV")).andExpect(status().isOk())
-                .andExpect(content().contentType("application/json;charset=UTF-8"))
-                
-
-    }*/
 }
