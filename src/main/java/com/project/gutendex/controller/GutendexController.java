fdd7024f4e057b655546c734c0acbc0e745642b9
GURRA Ina <ina.gurra@akka.eu>
2021-12-12 22:56:25 +0100

    configuring external log file

--- src/main/java/com/project/gutendex/controller/GutendexController.java
+++ src/main/java/com/project/gutendex/controller/GutendexController.java
@@ -6,6 +6,8 @@ import com.opencsv.exceptions.CsvDataTypeMismatchException;
 import com.opencsv.exceptions.CsvRequiredFieldEmptyException;
 import com.project.gutendex.entity.Book;
 import com.project.gutendex.entity.ResultBooks;
+import org.apache.log4j.Logger;
+import org.apache.log4j.PropertyConfigurator;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
@@ -16,13 +18,17 @@ import java.io.Writer;
 
 @RestController
 public class GutendexController {
-
-    @GetMapping(value = "/books")
+    @GetMapping(value = "/downloadCSV")
     public static void getBooks() throws IOException, CsvRequiredFieldEmptyException, CsvDataTypeMismatchException {
+        PropertyConfigurator.configure("log4j.properties");
+        Logger logger = Logger.getLogger(GutendexController.class);
+
+        logger.info("Using RestTemplate to call gutendex api");
         String url = "https://gutendex.com/books?topic=history";
         RestTemplate restTemplate = new RestTemplate();
-
         ResultBooks results = restTemplate.getForObject(url, ResultBooks.class);
+
+        logger.info("Ordering books in descending order");
         if (results != null) {
             //ordering
             results.getResults().sort((book1, book2)
@@ -30,6 +36,7 @@ public class GutendexController {
                     book1.getDownload_count()));
         }
         //CSV download
+        logger.info("Starting CSV download");
         String outputFileName = "Books.csv";
         try (Writer writer = new FileWriter(outputFileName)) {
             StatefulBeanToCsv<Book> statefulBeanToCsv = new StatefulBeanToCsvBuilder<Book>(writer).build();
